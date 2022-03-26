cb65534c552dc51b1edf1a490874c04c172ff497
sabrinaaraujoo <cien.sabrinaaraujo@outlook.com>
2022-03-26 01:53:23 -0300

    Navbar e footer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
+import {BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom'
+import Home from './components/pages/Home'
+import Company from './components/pages/Company'
+import Contact from './components/pages/Contact'
+import NewProject from './components/pages/NewProject'
+
+import Container from './components/layout/Container'
+
 function App() {
   return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
+    <Router>
+      <div>
+        <Link to='/'>Home</Link>
+        <Link to='/company'>Empresa</Link>
+        <Link to='/contact'>Contato</Link>
+        <Link to='/newproject'>Novo Projeto</Link>
+      </div>
+      <Container customClass="min-height">
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route exact path='/company' element={<Company />} />
+          <Route exact path='/contact' element={<Contact />} />
+          <Route exact path='/newproject' element={<NewProject />} />
+        </Routes>
+      </Container>
+      <p>Footer</p>
+    </Router>
   )
 }
 
