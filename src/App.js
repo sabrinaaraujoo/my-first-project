9dd28789bf7d77ab05fd4074054d3d223e83d6af
sabrinaaraujoo <cien.sabrinaaraujo@outlook.com>
2022-03-27 20:41:04 -0300

    Estilo Navbar e Footer

--- src/App.js
+++ src/App.js
@@ -1,29 +1,28 @@
 import {BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom'
 import Home from './components/pages/Home'
 import Company from './components/pages/Company'
+import Projects from './components/pages/Projects'
 import Contact from './components/pages/Contact'
 import NewProject from './components/pages/NewProject'
 
 import Container from './components/layout/Container'
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
 
 function App() {
   return (
     <Router>
-      <div>
-        <Link to='/'>Home</Link>
-        <Link to='/company'>Empresa</Link>
-        <Link to='/contact'>Contato</Link>
-        <Link to='/newproject'>Novo Projeto</Link>
-      </div>
+      <Navbar />
       <Container customClass="min-height">
         <Routes>
           <Route exact path='/' element={<Home />} />
           <Route exact path='/company' element={<Company />} />
+          <Route exact path='/projects' element={<Projects />} />
           <Route exact path='/contact' element={<Contact />} />
           <Route exact path='/newproject' element={<NewProject />} />
         </Routes>
       </Container>
-      <p>Footer</p>
+      <Footer />
     </Router>
   )
 }
