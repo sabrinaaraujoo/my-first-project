db907933b093bf77233ebb0da139b1f80e794951
sabrinaaraujoo <cien.sabrinaaraujo@outlook.com>
2022-03-28 20:35:16 -0300

    Home do projeto

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -10,7 +10,7 @@ function Navbar() {
         <nav className={styles.navbar}>
             <Container>
                 <Link to="/">
-                    <img src={logo} alt="Costs" />
+                    <img className={styles.img} src={logo} alt="Costs" />
                 </Link>
                 <ul className={styles.list}>
                     <li className={styles.item}>
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,5 +1,17 @@
+import styles from './Home.module.css'
+import savings from '../../img/savings.svg'
+
+import LinkButton from '../layout/LinkButton';
+
 function Home() {
-    return <h1>Home</h1>
+    return (
+        <section className={styles.home_container}>
+            <h1>Bem-Vindo <span>Costs</span></h1>
+            <p>Comece a gerenciar os seus porjetos agora mesmo!</p>
+            <LinkButton to="/newproject" text="Criar Projeto" />
+            <img src={savings} alt="Costs" />
+        </section>
+    )
 }
 
 export default Home;
